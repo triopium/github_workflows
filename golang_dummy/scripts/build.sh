217b1dc05ba0b3890f0b7434bb1f3ad06b0b525e
Jan Kacaba <jan.kacaba@gmail.com>
2024-01-19 11:34:26 +0100

    update workflow

--- golang_dummy/scripts/build.sh
+++ golang_dummy/scripts/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -x
 # Build the Go binary with linked variables.
 #NOTE: -ldflags can modify directly only simple variables i.e. it cannot directly modify struct fields.
-BuildTime="$(date +%FT%T)"
-GitCommit="$(git rev-parse HEAD)"
-GitTag="$(git describe --tags --abbrev=0)"
-path="github/triopium/github_workflows/golang_dummy/cmd"
-go build -ldflags "-X $path.BuildGitTag=$GitTag -X $path.BuildGitCommit=$GitCommit -X $path.BuildBuildTime=$BuildTime" -o golang_dummy.bin
+BUILD_TIME="$(date +%FT%T)"
+GIT_COMMIT="$(git rev-parse HEAD)"
+GIT_TAG="$(git describe --tags --abbrev=0)"
+GOSRC_PATH="github/triopium/github_workflows/golang_dummy/cmd"
+BINARY_NAME="${PWD##*/}"
+
+declare -a GOSRC_VAR=(
+"-X $GOSRC_PATH.BuildGitTag=$GIT_TAG"
+"-X $GOSRC_PATH.BuildGitCommit=$GIT_COMMIT"
+"-X $GOSRC_PATH.BuildBuildTime=$BUILD_TIME"
+)
+go build -ldflags "${GOSRC_VAR[*]}" -o "$BINARY_NAME"
